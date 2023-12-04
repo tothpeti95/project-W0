21615a186c07527ae634997ab9e3d38a1d4f043e
Benkő Pál <benko.pal90@gmail.com>
2023-12-04 14:49:40 +0100

    reworking files

--- BClass.java
+++
@@ -1,9 +0,0 @@
-public class BClass {
-    public static void main(String[] args) {
-        count(28);
-    }
-
-    public static void count(int age){
-        System.out.println(age);
-    }
-}
--- src/main.java
+++ src/main.java
@@ -3,9 +3,18 @@ package src;
 public class main {
     public static void main(String[] args) {
         count(28);
+        future(10);
     }
 
     public static void count(int age){
         System.out.println(age);
+    }
+
+    public static void future(int month){
+        if(month == 10){
+            System.out.println("Mérleg");
+        }else {
+            System.out.println("Nem mérleg");
+        }
     }
 }
