ae0480909cfbbc042c3b59227c5beedacfead96d
Fogas <fogas.marton@gmail.com>
2023-12-04 17:40:41 +0100

    a6

--- src/main.java
+++ src/main.java
@@ -2,14 +2,14 @@ package src;
 
 public class main {
     public static void main(String[] args) {
-        count(5);
+        count(5, 15);
         future(10);
         greet("Marci", "hello");
         cheer("madzsar");
     }
 
-    public static void count(int number){
-        System.out.println(number);
+    public static void count(int number, int number2){
+        System.out.println(number + number2);
     }
 
     public static void future(int month){
